931189fb10c4f29ae2a2efe136188ec7f4c647f2
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-23 15:31:18 -0500

    use case save file

---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+export interface SaveFileUseCase{
+   execute: ( options: Options) => boolean;
+}
+
+export interface Options {
+   fileContent: string;
+   fileDestination?: string;
+   fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+   constructor() {
+      /**
+       *  repository: Storage repository
+       */
+   }
+
+   execute = (options: Options): boolean => {
+
+      try {
+         const { fileContent, fileDestination = 'outputs', fileName = 'table' } = options;
+         fs.mkdirSync( fileDestination, { recursive: true } );
+         fs.writeFileSync(`${fileDestination}/${fileName}.txt`,  fileContent);
+         return true;
+      } catch (error) {
+         console.log('<--------------- JK Save-file.use-case --------------->');
+         console.log(error);
+         return false;
+      }
+      
+   };
+}
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
    base: number;
@@ -10,7 +11,16 @@ export class ServerApp {
    static  run( options: RunOptions ) {
       const { base, limit, showTable } = options;
       const table = new CreateTable().execute({ base, limit });
+      const wasCreated = new SaveFile()
+         .execute( { 
+            fileContent: table, 
+            fileDestination: `outputs/table-${base}` 
+         });
       console.log('<--------------- JK Server-app --------------->');
       if ( showTable ) console.log(table);
+
+      ( wasCreated )
+         ? console.log('file created')
+         : console.log('File not created');
    }
 }
