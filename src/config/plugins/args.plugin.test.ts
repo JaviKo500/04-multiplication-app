b541db4bc90312f6e42cb3c51d12ffb7940972b2
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-24 21:22:00 -0500

    test custom values in argv

--- src/config/plugins/args.plugin.test.ts
+++ src/config/plugins/args.plugin.test.ts
@@ -5,6 +5,12 @@ const runCommand = async (args: string []) => {
 }
 
 describe('Args.plugin.test', () => {
+   const originalArgv = process.argv;
+   beforeEach(() => {
+      process.argv = originalArgv;
+      jest.resetModules();
+   })
+
    test( 'should return default values', async () => {
       const yarg = await runCommand([ '-b', '5' ]);
 
@@ -16,6 +22,22 @@ describe('Args.plugin.test', () => {
             n: 'multiplication-table',
             d: 'outputs',
          })
+      );
+   });
+
+   test( 'should return configuration with custom values', async  () => {
+      const options = {
+         l: 20,
+         b: 2,
+         s: true,
+         n: 'table-2',
+         d: 'outputs',
+      }
+      const yarg = await runCommand([ '-b', options.b.toString(), '-l', options.l.toString(), '-s', '-n', options.n, '-d', options.d  ]);
+
+
+      expect( yarg ).toEqual(
+         expect.objectContaining(options)
       );
    });
 });
